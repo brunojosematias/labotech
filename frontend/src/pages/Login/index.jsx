88bc924ba1132514301f8bce89b37bb3695269b0
Bruno José <bru.matias100@gmail.com>
2023-05-07 11:25:19 -0300

    ajuste na tela de login

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,63 +1,69 @@
 import React from 'react';
 
+import aline from '../../assets/images/aline.svg';
+
 
 export function Login() {
   return (
     <>
-      <div className="">
-        <p className="">Bem-Vindo(a) de volta ao <span className="">LaboTech</span>!</p>
-        <p className="">Os <span>LaboInstrutores</span> estavam com saudades</p>
-      </div>
-
-      <div className="">
-        <a href="https://imgbb.com/%22%3E">
-          <img src="https://i.ibb.co/NCfBmHT/image.png" alt="Aline" border="0" />
+    <section className= "h-screen px-7 bg-background">
+      <div className="flex items-center">
+        <a href="https://imgbb.com/" className="w-10 mr-1.5">
+          <img src="https://i.ibb.co/3yp240w/image.png" alt="image" border="0"/>
         </a>
-
-        <p className=""><span>Aline</span> te dá boas-vindas!</p>
+        <h3 className="text-orange text-3xl">LaboTech</h3>
       </div>
 
       <div className="">
-          <h2 className="">Login</h2>
+        
+      </div>
+
+      <div className="flex items-center justify-between">
+      <div className="text-orange">
+          <p className="text-white font-extralight text-center">Bem-Vindo(a) de volta ao <span className="text-orange font-normal">LaboTech</span>!<br/>
+            Os <span className="text-orange">LaboInstrutores</span> estavam com saudades</p>
+          <img src={aline} alt="Aline" border="0" />
+        <p className="text-white font-medium"><span className="text-orange">Aline</span> te dá boas-vindas!</p>
+      </div>
+
+      <div className="">
+          <h2 className="text-orange font-medium text-2xl">Login</h2>
 
           <div className="">
-            <form className="" action="#" method="POST">
-              <div>
-                <div className="">
+            <form className="flex flex-col" action="#" method="POST">
                   <input type="email" name="email" value="" placeholder="E-mail" id="email" autoComplete="email"
-                  required  className="" />
-                </div>
-              </div>
+                  required  className="bg-background border rounded-3xl mb-7 py-3 pl-8"/>
 
-              <div>
-                <div className="">
-                </div>
-                <div className="">
                   <input type ="password" name="password" value="" placeholder="Senha" id="password" autoComplete="current-password"
-                  required  className=""/>
-                </div>
+                  required className="bg-background border rounded-3xl mb-4 py-3 pl-8"/>
 
-                <div className="">
-                    <input type="checkbox" value="lsRememberMe" id="rememberMe" /> <label for="rememberMe">Lembrar de mim</label>
+              <div className="flex">
+                <div className="text-white font-extralight">
+                    <input type="checkbox" value="lsRememberMe" id="rememberMe"/> <label for="rememberMe">Lembrar de mim</label>
                 </div> 
 
-                <div className="">
-                    <a href="#" className="">Esqueceu a senha?</a>
+                <div  className="text-white underline font-bold ml-24">
+                    <a href="#">Esqueceu a senha?</a>
                   </div>
               </div>
-
-              <div>
-                <button type="submit"className="">
+              
+              <button type="submit" className="w-full text-orange text-center bg-background font-medium border-2 border-orange my-5 rounded-3xl py-3 hover:bg-orange hover:text-black transition-all">
                   Entrar
-                </button>
-              </div>
+              </button>
             </form>
 
-            <p className="">Ainda não é membro?
-              <a href="#" className="" id="cadastrar">Cadastre-se agora mesmo!</a>
+            <div className="font-extralight">
+            <p className="text-white">Ainda não é membro?
+              <a href="#" className="text-orange underline" id="cadastrar">Cadastre-se agora mesmo!</a>
             </p>
+            </div>
           </div>
         </div>
+      </div>
+
+
+   
+    </section>
     </>
   )
 }
