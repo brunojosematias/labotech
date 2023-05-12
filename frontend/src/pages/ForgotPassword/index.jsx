dd0174112fa7519c5afd9a163f4aa2147679d121
Kawanny <kawannyviturino@gmail.com>
2023-05-12 18:12:09 -0300

    Últimas correções

--- frontend/src/pages/ForgotPassword/index.jsx
+++ frontend/src/pages/ForgotPassword/index.jsx
@@ -28,7 +28,7 @@ export function ForgotPassword() {
 
               <form className="flex flex-col font-extralight text-orange pr-60" action="#" method="POST">
                 <input type="email" name="email" placeholder="E-mail" id="email" autoComplete="email"
-                 required className="bg-background border rounded-3xl mb-3 py-3 pl-8 w-96 focus:outline-none focus:border-orange transition-all"/>
+                 required className="bg-background border rounded-3xl mb-3 py-3 pl-8 w-full focus:outline-none focus:border-orange transition-all"/>
 
                 <div className="flex gap-2 items-center mb-8">
                   <input type="text" name="rescueCode" placeholder="Código de Resgate" id="rescueCode" pattern="\d{6}" required className="bg-background border rounded-3xl w-52 py-3 pl-5 focus:outline-none focus:border-orange transition-all"/>
@@ -37,13 +37,11 @@ export function ForgotPassword() {
                     <Button>Verificar</Button>
                   </div>
                 </div>
-
-                <div>
+                
                   <Button>Renovar senha</Button>
-                </div>
               </form>
 
-              <div className="text-center font-extralight pr-60 mt-6">
+              <div className="text-center font-extralight pr-60 mt-8">
                 <p className="text-white">Ainda com problemas para recuperar sua conta?</p>
                   <a href="#" className="text-orange underline ml-1 hover:text-white" id="cadastrar">Entre em contato com o suporte aqui.</a>
               </div>
