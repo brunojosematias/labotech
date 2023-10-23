1e8b8a87ee9682494b1431cda4757286d7060f7d
Kawanny <kawannyviturino@gmail.com>
2023-10-23 18:36:57 -0300

    Responsividade da tela de recuperar a senha

--- backend/prisma/migrations/20231016215623_/migration.sql
+++ backend/prisma/migrations/20231016215623_/migration.sql
@@ -52,8 +52,18 @@ CREATE TABLE "exercises" (
     "level" INTEGER NOT NULL,
     "demonstration" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "CompletedExercisesId" TEXT NOT NULL,
 
     CONSTRAINT "exercises_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "NameExercises" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "ExercisesId" TEXT NOT NULL,
+
+    CONSTRAINT "NameExercises_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -77,6 +87,12 @@ ALTER TABLE "refresh_token" ADD CONSTRAINT "refresh_token_userId_fkey" FOREIGN K
 
 -- AddForeignKey
 ALTER TABLE "anamnesis_form" ADD CONSTRAINT "anamnesis_form_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "exercises" ADD CONSTRAINT "exercises_CompletedExercisesId_fkey" FOREIGN KEY ("CompletedExercisesId") REFERENCES "completed_exercises"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "NameExercises" ADD CONSTRAINT "NameExercises_ExercisesId_fkey" FOREIGN KEY ("ExercisesId") REFERENCES "exercises"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "completed_exercises" ADD CONSTRAINT "completed_exercises_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
