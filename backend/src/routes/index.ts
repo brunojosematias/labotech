c443c0e461af85ad73c99ca63f7c17815ab83950
Maria Iara <mariaiarasbraga@gmail.com>
2023-11-02 21:30:25 -0300

    feat: edição de senha com código de verificação

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,12 +3,14 @@ import { anamnesisRouter } from "./anamnesisForm.routes";
 import { authenticateRoutes } from "./authenticate.routes";
 import { exerciseRoutes } from "./exercises.routes";
 import { completedExercisesRouter } from "./completedExercises.routes";
+import { usersRouter } from "./users.routes";
 
 const router = Router();
 
+router.use(usersRouter)
+router.use(authenticateRoutes);
 router.use(exerciseRoutes);
 router.use(anamnesisRouter);
 router.use(completedExercisesRouter);
-router.use(authenticateRoutes);
 
 export { router };
