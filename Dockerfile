e71ca308ab5fc16def08d350e883deb08cab5b5e
Lucas Wedney <devwedney@outlook.com>
2023-11-30 18:47:26 -0300

    Revert "lucas nginx"
    This reverts commit cc06c665c6146130e390eb0e3381cb635c0d2b07.

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# FROM node:14
+
+# WORKDIR /app
+
+# COPY . .
+
+# RUN npm install
+
+# EXPOSE 3000
+
+# CMD ["node", "app.js"]
+
--- frontend/src/pages/AnamnesisForm/steps/ThirdForm.tsx
+++ frontend/src/pages/AnamnesisForm/steps/ThirdForm.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import { Button } from "../../../components/Button";
 import { ButtonSubmit } from "../../../components/ButtonSubmit";
 
 interface IStepInitial {
